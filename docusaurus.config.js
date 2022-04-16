3a97a4116fd42d1cbe91463a4555ac815ef1a1f2
p-acid <qkrtkstjd01@gmail.com>
2022-04-16 14:13:21 +0900

    init conifg

---
+++ docusaurus.config.js
@@ -0,0 +1,109 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: "Clean Code 💻",
+  tagline: "Summary of the Clean Code",
+  url: "https://github.com/p-acid/clean-code",
+  baseUrl: "/",
+  onBrokenLinks: "ignore",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/favicon.jpeg`",
+  organizationName: "p-acid", // Usuaㄴlly your GitHub org/user name.
+  projectName: "clean-code", // Usually your repo name.
+
+  presets: [
+    [
+      "classic",
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve("./sidebars.js"),
+          // Please change this to your repo.
+          editUrl: "https://github.com/p-acid/clean-code",
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          editUrl: "https://github.com/p-acid/clean-code",
+        },
+        theme: {
+          customCss: require.resolve("./src/css/custom.css"),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: "Clean Code",
+        logo: {
+          alt: "clean code logo",
+          src: "img/favicon.jpeg",
+        },
+        items: [
+          {
+            type: "doc",
+            docId: "intro",
+            position: "left",
+            label: "Docs",
+          },
+          { to: "/blog", label: "Blog", position: "left" },
+          {
+            href: "https://github.com/p-acid",
+            label: "GitHub",
+            position: "right",
+          },
+        ],
+      },
+      footer: {
+        style: "dark",
+        links: [
+          {
+            title: "Information",
+            items: [
+              {
+                label: "Book",
+                to: "https://www.aladin.co.kr/shop/wproduct.aspx?ItemId=34083680",
+              },
+              {
+                label: "Library",
+                to: "https://docusaurus.io/ko/docs/next",
+              },
+            ],
+          },
+          {
+            title: "Community",
+            items: [
+              {
+                label: "Twitter",
+                href: "https://twitter.com/Ac_idi_ty",
+              },
+            ],
+          },
+          {
+            title: "More",
+            items: [
+              {
+                label: "Blog",
+                to: "https://acid-log.vercel.app/",
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} Clean Code, Inc. Built with Docusaurus.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
---
+++ src/components/HomepageFeatures/index.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function HomepageFeatures(): JSX.Element {
+  return <section>hello</section>;
+}
